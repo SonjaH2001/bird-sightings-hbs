811ccc526854b8bc3baff4d1ec559c881deb3900
SH <email@email.com>
2017-04-06 19:38:07 -0500

    updates and comments

--- hbshelpers/helpers.js
+++ hbshelpers/helpers.js
@@ -1,10 +1,12 @@
 var moment = require('moment');
+// moment is a date thingy
 
 function dateFormat(date) {
-  m = moment.utc(date);
+  m = moment.utc(date);//utc=universal time
+    //parse the date, then format output
   return m.parseZone().format("dddd, MMMM Do YYYY, h:mm a");
 }
-
+// helpers manipulate data for display
 var helpers = {
   dateFormatter : dateFormat
 };
--- models/bird.js
+++ models/bird.js
@@ -1,16 +1,20 @@
 var mongoose = require('mongoose');
-var uniqueValidator = require('mongoose-unique-validator');
+var uniqueValidator = require('mongoose-unique-validator');//this is a plug in for Mongoose
 
 
 /* Represents a bird species */
+// these atributes will be added to the db
+
+//this is server side stuff, fyi
 var birdSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true, uniqueCaseInsensitive: true },
   description: String,
-  averageEggsLaid: {type: Number, min: 1, max: 50 },
+  averageEggsLaid: {type: Number, min: 1, max: 50 },//no negative numbers
   threatened: { type: Boolean, default: false },   // Is species endangered?
   datesSeen: [ { type: Date, default: Date.now, validate: {
     validator : function(date) {
     //return false if date is in the future
+        //now is UNIX time, same for everyone.
       return (date.getTime() < Date.now()) ; //time is less than now, in past
     }, message: '{VALUE} is not a valid sighting date. Date must be in the past'
   }} ],
